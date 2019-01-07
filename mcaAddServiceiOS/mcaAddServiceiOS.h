58ecd5c2dbb838988a5a17be2c46114b19dee115
Omar Israel Trujillo Osornio <otrujillo201513@gmail.com>
2019-01-07 16:22:19 -0600

    plist

---
+++ mcaAddServiceiOS/mcaAddServiceiOS.h
@@ -0,0 +1,19 @@
+//
+//  mcaAddServiceiOS.h
+//  mcaAddServiceiOS
+//
+//  Created by Omar Israel Trujillo Osornio on 1/7/19.
+//  Copyright © 2019 Speedy Movil. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for mcaAddServiceiOS.
+FOUNDATION_EXPORT double mcaAddServiceiOSVersionNumber;
+
+//! Project version string for mcaAddServiceiOS.
+FOUNDATION_EXPORT const unsigned char mcaAddServiceiOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <mcaAddServiceiOS/PublicHeader.h>
+
+
